3f93560ddf3b89b272014e3cbd3b2c4a163a9132
James Hurley <james.hurley@exordo.com>
2018-01-20 14:00:47 +0000

    Added activities, layouts and updated manifest.

--- app/src/main/java/com/example/hurley/codeninja/MainActivity.java
+++ app/src/main/java/com/example/hurley/codeninja/MainActivity.java
@@ -3,24 +3,23 @@ package com.example.hurley.codeninja;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.hurley.codeninja.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+    public void createNote(View view) {
+        Intent intent = new Intent(this, ViewActivity.class);
         startActivity(intent);
     }
 }
