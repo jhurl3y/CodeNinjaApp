bfce2f7437c8481e20b303035bc82bfc3d6bd1fd
James Hurley <james.hurley@exordo.com>
2018-01-21 00:18:23 +0000

    Using relative layouts.

--- app/src/main/java/com/example/hurley/codeninja/MainActivity.java
+++ app/src/main/java/com/example/hurley/codeninja/MainActivity.java
@@ -13,13 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarMain);
         setSupportActionBar(toolbar);
     }
 
     /** Called when the user taps the Send button */
     public void createNote(View view) {
-        Intent intent = new Intent(this, ViewActivity.class);
+        Intent intent = new Intent(this, EditActivity.class);
         startActivity(intent);
     }
 }
